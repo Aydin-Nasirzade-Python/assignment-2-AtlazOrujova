b43f6ddaf8bd68446b342722e1fed39f57a4a8d8
AtlazOrujova <165640684+AtlazOrujova@users.noreply.github.com>
2024-04-07 22:33:00 +0400

    Update Task_3.py

--- Task_3.py
+++ Task_3.py
@@ -1,9 +1,21 @@
 #import libraries here
 
 def main():
-  '''
-  Kodunuzu buraya yazin.
-  '''
+  a=int(input("Enter the wavelength in nm: "))
+  if 620<=a<750:
+    print("The relevant color is Red")
+  elif 590<=a<620：
+    print("The relevant color is Orange")
+  elif 570<=a<590:
+    print("The relevant color is Yellow")
+  elif 495<=a<570：
+    print("The relevant color is Green")
+  elif 450<=a<495:
+    print("The relevant color is Blue")
+  elif 380<=a<450:
+    print("The relevant color is Violet")
+  else:
+    print("Invalid input!")
   pass
 
 if __name__ == "__main__":
